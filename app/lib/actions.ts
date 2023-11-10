36e62e470d946b23c553388c29c56077b1b58f32
Javi Cuenca <jcuencagento@gmail.com>
2023-11-10 20:12:33 +0100

    Avoid login...

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,7 +4,6 @@ import { z } from 'zod';
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { signIn } from '@/auth';
 
 const FormSchema = z.object({
   id: z.string(),
@@ -119,16 +118,3 @@ export async function deleteInvoice(id: string) {
   }
 }
 
-export async function authenticate(
-  prevState: string | undefined,
-  formData: FormData,
-) {
-  try {
-    await signIn('credentials', Object.fromEntries(formData));
-  } catch (error) {
-    if ((error as Error).message.includes('CredentialsSignin')) {
-      return 'CredentialsSignin';
-    }
-    throw error;
-  }
-}
